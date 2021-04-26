c68621d17d6b619c4c52b3ce7c56e8821a1092f8
Musakanya <74012430+Musakanya@users.noreply.github.com>
2021-04-26 16:44:28 +0200

    Final

--- db/eventerity.sql
+++ db/eventerity.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2021 at 06:58 PM
+-- Generation Time: Apr 26, 2021 at 04:44 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -38,6 +38,7 @@ CREATE TABLE `events` (
   `country` varchar(50) NOT NULL,
   `price` int(11) NOT NULL,
   `event_description` varchar(150) NOT NULL,
+  `event_url` varchar(150) DEFAULT NULL,
   `tickets_available` int(11) DEFAULT NULL,
   `event_owner` varchar(100) DEFAULT NULL,
   `event_type` varchar(150) DEFAULT NULL,
@@ -49,9 +50,9 @@ CREATE TABLE `events` (
 -- Dumping data for table `events`
 --
 
-INSERT INTO `events` (`id`, `event_name`, `event_date`, `event_time`, `street_address`, `city`, `post_code`, `country`, `price`, `event_description`, `tickets_available`, `event_owner`, `event_type`, `participation_type`, `bought`) VALUES
-(16, 'Octoberfest 2021', '2021-04-15', '21:00:00', 'Woodlands plot 3223', 'Lusaka', 10101, 'zambia', 500, 'Ready to dance? Come to this year\'s Octoberfest Live!', 500, 'musakanyakapoma@gmail.com', 'festival', 'on_site', NULL),
-(17, 'Novemberfest 2021', '2021-04-14', '12:15:00', 'dfdsfds', 'Lusaka', 10101, 'zambia', 500, 'Novemberfest blah blah blah', 500, 'musakanyakapoma@gmail.com', 'festival', 'on_site', NULL);
+INSERT INTO `events` (`id`, `event_name`, `event_date`, `event_time`, `street_address`, `city`, `post_code`, `country`, `price`, `event_description`, `event_url`, `tickets_available`, `event_owner`, `event_type`, `participation_type`, `bought`) VALUES
+(16, 'Octoberfest 2021', '2021-04-15', '21:00:00', 'Woodlands plot 3223', 'Lusaka', 10101, 'zambia', 500, 'Ready to dance? Come to this year\'s Octoberfest Live!', NULL, 499, 'musakanyakapoma@gmail.com', 'festival', 'on_site', 0),
+(18, 'Westminster', '2021-04-23', '12:00:00', 'Chilanga', 'Lusaka', 10101, 'zambia', 0, 'Work out', NULL, 0, 'test@email.com', 'conference', 'on_site', 7);
 
 -- --------------------------------------------------------
 
@@ -81,7 +82,7 @@ INSERT INTO `event_type` (`event_type`) VALUES
 --
 
 CREATE TABLE `order_history` (
-  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
   `event_id` int(11) DEFAULT NULL,
   `order_date` date NOT NULL,
   `user` varchar(150) DEFAULT NULL
@@ -91,8 +92,53 @@ CREATE TABLE `order_history` (
 -- Dumping data for table `order_history`
 --
 
-INSERT INTO `order_history` (`id`, `event_id`, `order_date`, `user`) VALUES
-(1, 17, '2021-04-15', 'sumi@gmail.com');
+INSERT INTO `order_history` (`order_id`, `event_id`, `order_date`, `user`) VALUES
+(0, 18, '2021-04-19', 'sumi@gmail.com'),
+(1, 18, '2021-04-19', 'sumi@gmail.com'),
+(1001, 18, '2021-04-19', 'sumi@gmail.com'),
+(1002, 18, '2021-04-19', 'sumi@gmail.com'),
+(1003, 18, '2021-04-19', 'sumi@gmail.com'),
+(1004, 18, '2021-04-19', 'sumi@gmail.com'),
+(1005, 16, '2021-04-19', 'sumi@gmail.com'),
+(1009, 18, '2021-04-19', 'sumi@gmail.com'),
+(10000, 16, '2021-04-06', 'sumi@gmail.com'),
+(40398, 18, '2021-04-19', 'sumi@gmail.com'),
+(57192, 18, '2021-04-19', 'sumi@gmail.com'),
+(22535, 18, '2021-04-19', 'sumi@gmail.com'),
+(98467, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(20997, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(80543, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(46216, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(87964, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(80367, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(59229, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(11782, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(55667, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(35593, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(66093, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(49271, 18, '2021-04-19', 'musakanyakapoma@gmail.com'),
+(23199, 18, '2021-04-21', 'musakanyakapoma@gmail.com'),
+(64142, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(32893, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(51248, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(62570, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(50109, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(69641, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(80055, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(18467, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(88391, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(57738, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(57683, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(72924, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(11985, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(65900, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(53936, 18, '2021-04-22', 'musakanyakapoma@gmail.com'),
+(84166, 18, '2021-04-23', 'musakanyakapoma@gmail.com'),
+(59296, 18, '2021-04-23', 'musakanyakapoma@gmail.com'),
+(55335, 18, '2021-04-23', 'musakanyakapoma@gmail.com'),
+(33997, 18, '2021-04-23', 'musakanyakapoma@gmail.com'),
+(66273, 18, '2021-04-23', 'musakanyakapoma@gmail.com'),
+(63051, 18, '2021-04-23', 'musakanyakapoma@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -132,28 +178,12 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`email`, `password`, `first_name`, `last_name`, `phone_no`, `user_type`) VALUES
-('musakanyakapoma@gmail.com', '$2y$10$i9uE0Yz./NTwogl2jBmQOeuGJlLnIO8bPMyLAFeNGOF/ubNEelEfS', 'Musakanya', 'Kapoma', '', 'US'),
-('sumi@gmail.com', '$2y$10$tNWS65ULdkueqMf0iF8KuOqaCdPaUa7kDXuszkbUJur.JTO4jTV8y', 'Sumi', 'M', '', 'US'),
-('test@email.com', '$2y$10$dJboWm/U9v1DgndWBPjDKOMd6f0ZJ6iXnlUxmXD9fA0.acNKB0LdC', 'test', 'test', '', 'US');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_on_event`
---
-
-CREATE TABLE `user_on_event` (
-  `id` int(11) NOT NULL,
-  `email` varchar(150) DEFAULT NULL,
-  `event_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user_on_event`
---
-
-INSERT INTO `user_on_event` (`id`, `email`, `event_id`) VALUES
-(4, 'sumi@gmail.com', 17);
+('admin@eventerity.com', '$2y$10$adqzRRZZ1q5ZdJ08fcCf9ekw9ezQ4lEAl.drkStA6m3nlnuwRHdhe', 'Nchima', 'Kapoma', '096-585-7131', 'AD'),
+('kapomamusa@gmail.com', '$2y$10$dixRWFIJTLG0k4bDoGfo/ONTlv/NcPz3vQ.ZKLfp0JN441zoqqt9C', 'Musa', 'Kapoma', '445-778-7744', 'US'),
+('musakanyakapoma@gmail.com', '$2y$10$i9uE0Yz./NTwogl2jBmQOeuGJlLnIO8bPMyLAFeNGOF/ubNEelEfS', 'Musakanya', 'Kapoma', '778-777-6666', 'US'),
+('sumi@gmail.com', '$2y$10$tNWS65ULdkueqMf0iF8KuOqaCdPaUa7kDXuszkbUJur.JTO4jTV8y', 'Sumi', 'M', '999-444-7441', 'US'),
+('test@email.com', '$2y$10$dJboWm/U9v1DgndWBPjDKOMd6f0ZJ6iXnlUxmXD9fA0.acNKB0LdC', 'Thodor', 'Smith', '', 'US'),
+('tsukki@gmail.com', '$2y$10$OeO2TeSqIxQq8W70CfSmbO4kqeIfGYC2V.oKhFedBY2RSs.AhuSNy', 'Kei', 'Tsukishima', '', 'US');
 
 -- --------------------------------------------------------
 
@@ -198,7 +228,6 @@ ALTER TABLE `event_type`
 -- Indexes for table `order_history`
 --
 ALTER TABLE `order_history`
-  ADD PRIMARY KEY (`id`),
   ADD KEY `event_id` (`event_id`),
   ADD KEY `user` (`user`);
 
@@ -216,14 +245,6 @@ ALTER TABLE `user`
   ADD KEY `user_type` (`user_type`);
 
 --
--- Indexes for table `user_on_event`
---
-ALTER TABLE `user_on_event`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `event_id` (`event_id`),
-  ADD KEY `email` (`email`);
-
---
 -- Indexes for table `user_type`
 --
 ALTER TABLE `user_type`
@@ -237,19 +258,7 @@ ALTER TABLE `user_type`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- AUTO_INCREMENT for table `order_history`
---
-ALTER TABLE `order_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
-
---
--- AUTO_INCREMENT for table `user_on_event`
---
-ALTER TABLE `user_on_event`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
@@ -275,13 +284,6 @@ ALTER TABLE `order_history`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_type`) REFERENCES `user_type` (`user_type`);
-
---
--- Constraints for table `user_on_event`
---
-ALTER TABLE `user_on_event`
-  ADD CONSTRAINT `user_on_event_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`),
-  ADD CONSTRAINT `user_on_event_ibfk_2` FOREIGN KEY (`email`) REFERENCES `user` (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
